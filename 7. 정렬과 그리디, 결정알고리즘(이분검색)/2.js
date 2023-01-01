ac95e67552989ecc30525989d91da1c556e47fe6
tkyeom96 <tkyeom96@gmail.com>
2023-01-01 20:41:20 +0900

    버블정렬

---
+++ 7. 정렬과 그리디, 결정알고리즘(이분검색)/2.js
@@ -0,0 +1,23 @@
+/**
+ * 2. 버블정렬
+ *
+ * 6
+ * 13 5 11 7 23 15
+ *
+ * 5 7 11 13 15 23
+ */
+
+function solution(arr) {
+  let answer = arr
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return answer
+}
+
+let arr = [13, 5, 11, 7, 23, 15]
+console.log(solution(arr))
