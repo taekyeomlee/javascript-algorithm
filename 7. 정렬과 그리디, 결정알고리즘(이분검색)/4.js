bbbea932767c38f3819a69a709d64e87a3007400
tkyeom96 <tkyeom96@gmail.com>
2023-01-03 21:39:02 +0900

    삽입정렬

---
+++ 7. 정렬과 그리디, 결정알고리즘(이분검색)/4.js
@@ -0,0 +1,32 @@
+/**
+ * 4. 삽입정렬
+ *
+ * 6
+ * 11 7 5 6 10 9
+ *
+ * 5 6 7 9 10 11
+ */
+
+function solution(arr) {
+  let answer = arr
+
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = arr[i]
+    let j
+
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) {
+        arr[j + 1] = arr[j]
+      } else {
+        break
+      }
+    }
+
+    arr[j + 1] = tmp
+  }
+
+  return answer
+}
+
+const arr = [11, 7, 5, 6, 10, 9]
+console.log(solution(arr))
