08938f27232d1fa31b4ee41792c88a5834066afe
tkyeom96 <tkyeom96@gmail.com>
2023-01-21 21:35:01 +0900

    leetcode: 700 (Tree, Binary Search Tree, Binary Tree)

---
+++ leetcode/700.js
@@ -0,0 +1,25 @@
+/**
+ * 700. Search in a Binary Search Tree > Tree, Binary Search Tree, Binary Tree
+ *
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+  if (!root) return null
+  if (root.val === val) {
+    return root
+  } else if (root.val > val) {
+    return searchBST(root.left, val)
+  } else {
+    return searchBST(root.right, val)
+  }
+}
