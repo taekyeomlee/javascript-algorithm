2e947bc2dac4e7571d77f9a0a0ea49a535649c75
Taekyeom Lee <tkyeom96@gmail.com>
2023-02-09 01:02:05 +0900

    leetcode: 200 (DFS, Matrix)

---
+++ leetcode/dfs_matrix/200.js
@@ -0,0 +1,27 @@
+/**
+ * 200. Number of Islands (Medium)
+ *
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  let cnt = 0
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c] === '1') {
+        dfs(grid, r, c)
+        cnt++
+      }
+    }
+  }
+  return cnt
+}
+
+function dfs(grid, r, c) {
+  if (r < 0 || c < 0 || r >= grid.length || c >= grid[r].length || grid[r][c] !== '1') return
+  grid[r][c] = 'X'
+  dfs(grid, r - 1, c)
+  dfs(grid, r + 1, c)
+  dfs(grid, r, c - 1)
+  dfs(grid, r, c + 1)
+}
