9b4d44618195c77e0d819603c7add8f59cb09af4
Taekyeom Lee <tkyeom96@gmail.com>
2023-02-16 01:49:42 +0900

    leetcode: 572 (DFS, Tree)

---
+++ leetcode/dfs_tree/572.js
@@ -0,0 +1,25 @@
+/**
+ * 572. Subtree of Another Tree (Easy)
+ *
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+  if (!root) return false
+  return isEqual(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+}
+
+function isEqual(root1, root2) {
+  if (!root1 || !root2) return !root1 && !root2
+  if (root1.val !== root2.val) return false
+  return isEqual(root1.left, root2.left) && isEqual(root1.right, root2.right)
+}
