41e8073b21d366bca8507a8316fa1a0bfccdae29
Taekyeom Lee <tkyeom96@gmail.com>
2023-03-07 01:59:25 +0900

    programmers: 43164 (DFS, Backtracking) - Graph 풀이 추가 작성

--- programmers/DFS_BFS/43164.js
+++ programmers/DFS_BFS/43164.js
@@ -27,3 +27,58 @@ function solution(tickets) {
 
   return result[0]
 }
+
+/**
+ * 방향성 Graph로 접근 (인접리스트)
+ *
+ * 1번 케이스가 중복된 티켓
+ * From -> To (중복된 티켓이 있어서 해당 경로를 여러번 가는 케이스가 있다.)
+ * key값을 idx를 추가하여 방문 여부를 파악하는 방법으로 해결.
+ *
+ * 예외 상황
+ * Graph[from] 접근시 undefined 케이스가 있다.
+ * 초기에 모든 Node의 인접리스트를 빈 배열로 초기화해서 해결.
+ */
+
+function solution(tickets) {
+  const result = []
+  const graph = {}
+  const visited = {}
+
+  const set = new Set(tickets.flat())
+  for (const node of set) {
+    graph[node] = []
+  }
+
+  for (const [from, to] of tickets) {
+    graph[from].push(to)
+  }
+
+  for (const from in graph) {
+    graph[from].sort()
+  }
+
+  const backtrack = (start, path) => {
+    if (path.length === tickets.length + 1) {
+      result.push([...path])
+      return
+    }
+
+    for (let i = 0; i < graph[start].length; i++) {
+      const next = graph[start][i]
+      const key = `${start}${i}`
+
+      if (visited[key]) continue
+
+      path.push(next)
+      visited[key] = true
+      backtrack(next, path)
+      visited[key] = false
+      path.pop()
+    }
+  }
+
+  backtrack('ICN', ['ICN'])
+
+  return result[0]
+}
