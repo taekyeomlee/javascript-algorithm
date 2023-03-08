a071302bfd8ccc83049b8e4ac41529a63529a8c7
Taekyeom Lee <tkyeom96@gmail.com>
2023-03-08 23:29:25 +0900

    leetcode: 207 (Graph, Topological Sort)

---
+++ leetcode/graph_topologicalSort/207.js
@@ -0,0 +1,39 @@
+/**
+ * 207. Course Schedule (Medium)
+ *
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+  const graph = Array.from({ length: numCourses }, () => [])
+  const inDegree = new Array(numCourses).fill(0)
+  const queue = []
+  const order = []
+
+  for (const [course, prereq] of prerequisites) {
+    graph[prereq].push(course)
+    inDegree[course]++
+  }
+
+  for (let i = 0; i < inDegree.length; i++) {
+    if (inDegree[i] === 0) {
+      queue.push(i)
+    }
+  }
+
+  while (queue.length) {
+    const curr = queue.shift()
+    order.push(curr)
+
+    for (const next of graph[curr]) {
+      inDegree[next]--
+
+      if (inDegree[next] === 0) {
+        queue.push(next)
+      }
+    }
+  }
+
+  return numCourses === order.length
+}
